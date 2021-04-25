31d009b4fb0e72ad4192e4b3cdc9deca720315ac
INMihaylov19 <INMihaylov19@codingburgas.bg>
2021-04-25 22:56:44 +0300

    Update info.js

--- Product/JS/info.js
+++ Product/JS/info.js
@@ -9,33 +9,33 @@ for (const sgnl of signali) {
 }
 
 if (count == 0) {
-    document.write("Не е наличен");
+    document.write("1010 не е наличен");
 }
 
 if (count >= 1) {
-    document.write("Не е наличен");
+    document.write("1011 не е наличен");
 }
 
 if (count >= 2) {
-    document.write("Не е наличен");
+    document.write("1100 не е наличен");
 }
 
 if (count >= 3) {
-    document.write("Не е наличен");
+    document.write("1101 не е наличен");
 }
 
 if (count >= 4) {
-    document.write("Не е наличен");
+    document.write("1110 не е наличен");
 }
 
 if (count >= 5) {
-    document.write("Не е наличен");
+    document.write("1111 не е наличен");
 }
 
 if (count >= 6) {
-    document.write("Не е наличен");
+    document.write("10000 не е наличен");
 }
 
 if (count >= 7) {
-    document.write("Не е наличен");
+    document.write("10001 не е наличен");
 }
