a357c26da0923518ec10eb1a385629a47de2737b
INMihaylov19 <INMihaylov19@codingburgas.bg>
2021-04-25 23:52:03 +0300

    Fix info.js

--- Product/JS/info.js
+++ Product/JS/info.js
@@ -1,41 +1,41 @@
-let signali = JSON.parse(localStorage.getItem("Signali"));
-console.log(signali);
+let signals = JSON.parse(window.localStorage.getItem("Signali"));
+console.log(signals);
 var count = 0;
-for (const sgnl of signali) {
+for (const sgnl of signals) {
   const sgnlc = Object.assign(sgnl);
   delete sgnlc.id;
   count++;
   document.body.innerHTML += `<p>${sgnlc.name} е изпратил сигнал за пожар на адрес ${sgnlc.adres} (телефонен номер - ${sgnlc.tnumber} )</p>`;
 }
 
-if (count == 0) {
-    document.write("1010 не е наличен");
-}
-
 if (count >= 1) {
-    document.write("1011 не е наличен");
+    document.getElementById("status1").innerHTML=("Не е наличен");
 }
 
 if (count >= 2) {
-    document.write("1100 не е наличен");
+    document.getElementById("status2").innerHTML=("Не е наличен");
 }
 
 if (count >= 3) {
-    document.write("1101 не е наличен");
+    document.getElementById("status3").innerHTML=("Не е наличен");
 }
 
 if (count >= 4) {
-    document.write("1110 не е наличен");
+    document.getElementById("status4").innerHTML=("Не е наличен");
 }
 
 if (count >= 5) {
-    document.write("1111 не е наличен");
+    document.getElementById("status5").innerHTML=("Не е наличен");
 }
 
 if (count >= 6) {
-    document.write("10000 не е наличен");
+    document.getElementById("status6").innerHTML=("Не е наличен");
 }
 
 if (count >= 7) {
-    document.write("10001 не е наличен");
+    document.getElementById("status7").innerHTML=("Не е наличен");
+}
+
+if (count >= 8) {
+    document.getElementById("status8").innerHTML=("Не е наличен");
 }
